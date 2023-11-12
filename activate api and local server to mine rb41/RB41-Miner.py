816cb70af8ccf14616ff7735d10169f1f8015cf9
rb41send <rb41send@gmail.com>
2023-11-12 18:23:52 -0300

    Adicionando novos arquivos e imagens

--- activate api and local server to mine rb41/RB41-Miner.py
+++ activate api and local server to mine rb41/RB41-Miner.py
@@ -35,22 +35,7 @@ if endereco_ip_roteador:
     MINING_SERVER_URLS = [f'http://{endereco_ip_roteador}:2083/api']
     SEND_BALANCE_SERVER_URLS = [f'http://{endereco_ip_roteador}:2083/api/enviar_saldo']
 
-    # Escolha uma URL aleatoriamente da lista de URLs de envio de saldo
-    selected_balance_server_url = random.choice(SEND_BALANCE_SERVER_URLS)
-
-    # Inicialize o iterator para as URLs de mineração
-    url_iterator = itertools.cycle(MINING_SERVER_URLS)
-
-    # Aguarde 15 segundos
-    print("Aguarde... Coletando informações do Peer-to-peer.")
-    for _ in range(15):
-        time.sleep(1)
-        print(".", end="", flush=True)  # Exibe uma barra de carregamento simples
-    print("\n\nInformações do Peer-to-peer coletadas com sucesso:")
-    print(f"Iniciado Peer-to-peer: {endereco_ip_roteador}")
-    print(f"Porta: 2083")
-else:
-    print("Não foi possível obter o endereço IP do roteador.")
+  
     
 # Adicione o nome da moeda como uma variável global
 nome_da_moeda = "RB41"
@@ -113,42 +98,8 @@ def mineracao_cpu(carteira_mineradora, last_proof, difficulty, cpu_limit):
             if cpu_current_percent > cpu_limit:
                 cpu_percent = cpu_limit
 
-            # Defina o uso da CPU
-            psutil.cpu_percent(interval=None)
-
-            # Fazer uma solicitação ao servidor para mineração
-            response = requests.post(next(url_iterator) + '/minerar', json={
-                'carteira': carteira_mineradora,
-                'last_proof': last_proof,
-                'difficulty': difficulty
-            })
-
-            if response.status_code == 200:
-                data = response.json()
-                recompensa = data.get("recompensa")
-                bloco_minerado = data.get("bloco_minerado")
-
-                if recompensa is not None:
-                    if bloco_minerado is not None:
-                        print(f"Mineração (CPU) concluída! Recompensa recebida em {nome_da_moeda}: {recompensa}")
-                        print(f"Bloco minerado: {bloco_minerado}")
-
-                        # Distribua a recompensa com base na potência de hash do minerador
-                        distribuir_recompensa(carteira_mineradora, recompensa)
-                    else:
-                        print(f"Mineração (CPU) concluída! Recompensa recebida em {nome_da_moeda}: {recompensa}, mas nenhum bloco foi minerado.")
-                else:
-                    print("Erro ao iniciar a mineração (CPU).")
-            else:
-                print("Erro na solicitação de mineração (CPU)")
-
-            # Aguarde um período de tempo (por exemplo, 60 segundos) antes de fazer a próxima solicitação
-            time.sleep(60)  # Isso fará com que haja um atraso de 60 segundos entre as solicitações
-    except Exception as e:
-        print("Erro durante a mineração CPU:", str(e))
-        time.sleep(15)  # Atraso de 60 segundos em caso de erro
-
-
+    
+          
 def mineracao_gpu(carteira_mineradora, last_proof, difficulty, gpu_limit=None):  # Adicione um argumento padrão para gpu_limit
     while True:
         print(f"Minerando com carteira (GPU): {carteira_mineradora}, last_proof: {last_proof}, difficulty: {difficulty}")
@@ -164,37 +115,6 @@ def mineracao_gpu(carteira_mineradora, last_proof, difficulty, gpu_limit=None): 
         if gpu_current_percent > gpu_percent:
             gpu_percent = gpu_limit
 
-        # Defina o uso da GPU
-        GPUtil.showUtilization()
-
-        # Fazer uma solicitação ao servidor para mineração
-        response = requests.post(next(url_iterator) + '/minerar', json={
-            'carteira': carteira_mineradora,
-            'last_proof': last_proof,
-            'difficulty': difficulty
-        })
-
-        if response.status_code == 200:
-            data = response.json()
-            recompensa = data.get("recompensa")
-            bloco_minerado = data.get("bloco_minerado")
-
-            if recompensa is not None:
-                if bloco_minerado is not None:
-                    print(f"Mineração (GPU) concluída! Recompensa recebida em {nome_da_moeda}: {recompensa}")
-                    print(f"Bloco minerado: {bloco_minerado}")
-
-                    # Distribua a recompensa com base na potência de hash do minerador
-                    distribuir_recompensa(carteira_mineradora, recompensa)
-                else:
-                    print(f"Mineração (GPU) concluída! Recompensa recebida em {nome_da_moeda}: {recompensa}, mas nenhum bloco foi minerado.")
-            else:
-                print("Erro ao iniciar a mineração (GPU).")
-        else:
-            print("Erro na solicitação de mineração (GPU)")
-
-        # Aguarde um período de tempo (por exemplo, 60 segundos) antes de fazer a próxima solicitação
-        time.sleep(15)  # Isso fará com que haja um atraso de 60 segundos entre as solicitações
 
 def registrar_cliente():
     senha = input("Digite a senha para a nova carteira: ")
@@ -226,8 +146,7 @@ def distribuir_recompensa(carteira_mineradora, recompensa):
 
 # Função para simular o tempo de mineração de um minerador
 def tempo_de_mineracao():
-    # Simule o tempo de mineração com valores aleatórios entre 300 e 600 segundos (5 a 10 minutos)
-    return random.randint(300, 600)
+
 
 def minerar(carteira):
     # Simule a mineração aqui (pode envolver cálculos complexos)
@@ -252,115 +171,13 @@ def enviar_saldo(carteira_origem, carteira_destino, valor, send_balance_server_u
         'carteira_destino': carteira_destino,
         'valor': valor,
         'senha_origem': senha_origem
-    }
-    
+    }    
     # Escolha uma URL aleatoriamente da lista de URLs de envio de saldo
-    selected_balance_server_url = random.choice(send_balance_server_urls)
 
-    response = requests.post(selected_balance_server_url, json=dados)
-    
-    if response.status_code == 200:
-        try:
-            data = response.json()
-            print("Saldo enviado com sucesso.")
-        except json.decoder.JSONDecodeError:
-            print("Erro: Resposta do servidor não contém dados JSON válidos.")
-    else:
-        print("Erro ao enviar saldo. Código de status:", response.status_code)
 
 # Função para salvar carteiras e saldos em um arquivo JSON
 def salvar_carteiras_e_saldos():
-    data = {
-        "carteiras": [{"endereco": c.endereco, "saldo": c.saldo, "poder_de_hash": c.poder_de_hash, "senha": c.senha} for c in carteiras],
-        "contador_blocos": contador_blocos
-    }
 
-    with open("carteiras_saldos.json", "w") as json_file:
-        json.dump(data, json_file)
 
 # Função para carregar carteiras e saldos de um arquivo JSON
 def carregar_carteiras_e_saldos():
-    global carteiras, contador_blocos
-
-    if os.path.exists("carteiras_saldos.json"):
-        with open("carteiras_saldos.json", "r") as json_file:
-            data = json.load(json_file)
-            carteiras_data = data.get("carteiras", [])
-            contador_blocos = data.get("contador_blocos", 0)
-
-            carteiras = [Carteira(c["endereco"], c["saldo"], c["poder_de_hash"], c["senha"]) for c in carteiras_data]
-
-# Carregar carteiras e saldos do arquivo JSON, se existir
-carregar_carteiras_e_saldos()
-
-if __name__ == "__main__":
-    carteira_mineradora = None  # Inicialmente, não há carteira mineradora
-
-    while True:
-        print("Escolha uma opção:")
-        print("1. Criar uma nova carteira")
-        print("2. Verificar saldo de carteira")
-        print("3. Iniciar Mineração Contínua")
-        print("4. Enviar saldo para outra carteira")
-        print("5. Sair")
-
-        opcao = input("Digite o número da opção desejada: ")
-
-        if opcao == "1":
-            endereco = criar_carteira()
-            if endereco:
-                print(f"Sua nova carteira foi criada com o endereço: {endereco}")
-                poder_de_hash = float(input("Digite o poder de hash da carteira (maior poder de hash, maior recompensa): "))
-                senha = input("Digite a senha da carteira: ")
-                carteiras.append(Carteira(endereco, 0.0, poder_de_hash, senha))
-                salvar_carteiras_e_saldos()
-            else:
-                print("Erro ao criar a carteira.")
-        elif opcao == "2":
-            carteira = input("Digite o endereço da carteira que deseja verificar: ")
-            saldo = verificar_saldo(carteira)
-            if saldo is not None:
-                print(f"Saldo da carteira {carteira}: {saldo}")
-            else:
-                print("Carteira não encontrada ou senha incorreta.")
-        elif opcao == "3":
-            if carteira_mineradora:
-                print(f"Mineração contínua já está ativa com a carteira {carteira_mineradora}...")
-            else:
-                carteira_mineradora = input("Digite o endereço da carteira para iniciar a mineração contínua: ")
-                last_proof = 0  # Inicialize com um valor adequado
-                difficulty = 4  # Defina a dificuldade desejada
-                cpu_limit = float(input("Digite o limite de uso da CPU (0-100%): "))
-                gpu_limit = float(input("Digite o limite de uso da GPU (0-100%): "))
-
-                # Crie threads separadas para a mineração de CPU e GPU
-                thread_cpu = threading.Thread(target=mineracao_cpu, args=(carteira_mineradora, last_proof, difficulty, cpu_limit))
-                thread_gpu = threading.Thread(target=mineracao_gpu, args=(carteira_mineradora, last_proof, difficulty, gpu_limit))
-
-                # Inicie as threads
-                thread_cpu.start()
-                thread_gpu.start()
-
-                # Aguarde até que ambas as threads terminem (isso não acontecerá neste exemplo, pois são loops infinitos)
-                # thread_cpu.join()
-                # thread_gpu.join()
-        elif opcao == "4":
-            carteira_origem = input("Digite o endereço da carteira de origem: ")
-            carteira_destino = input("Digite o endereço da carteira de destino: ")
-            valor = float(input("Digite o valor a ser enviado: "))
-            enviar_saldo(carteira_origem, carteira_destino, valor, SEND_BALANCE_SERVER_URLS)
-            salvar_carteiras_e_saldos()
-
-            for c in carteiras:
-                if c.endereco == carteira_origem:
-                    senha_origem = input("Digite a senha da carteira de origem: ")
-                    if senha_origem == c.senha:
-                        enviar_saldo(carteira_origem, carteira_destino, valor)
-                        salvar_carteiras_e_saldos()
-                    else:
-                        print("Senha incorreta. Operação cancelada.")
-                    break
-            else:
-                print("Carteira de origem não encontrada.")
-        elif opcao == "5":
-            sys.exit(0)
